ca38e6280720f148c2dcc5b1443a64a711712c67
Rajan Poudel <rajanpoudel.np@gmail.com>
2025-11-04 23:09:05 +0545

    Done

---
+++ products.js
@@ -0,0 +1,41 @@
+// products.js - Keep this file updated with your products
+
+const products = [
+    {
+        id: 1,
+        name: "GlobWeb",
+        image: "img/globweb.png",
+        price: "Paid",
+        link: "https://globsoftinc.github.io/GlobWeb/"
+    },
+    {
+        id: 2,
+        name: "GlobIntern",
+        image: "img/globintern.png",
+        price: "Free",
+        link: "https://globsoftinc.github.io/GlobIntern/"
+    },
+    {
+        id: 3,
+        name: "GlobFinance",
+        image: "img/globfinance.png",
+        price: "Free",
+        link: "https://github.com/globsoftinc/GlobFinance/releases/download/Published/bill_splitter.apk"
+    },
+    {
+        id: 4,
+        name: "GlobPass",
+        image: "img/globpass.png",
+        price: "Free",
+        link: "https://globsoftinc.github.io/GlobPass/"
+    },
+    {
+        id: 5,
+        name: "Our App",
+        image: "img/ourapp.png",
+        price: "Coming Soon",
+        link: null
+    }
+];
+
+export default products;
